6ae9a303f34dab31b4271a581137d943d11722ec
Jason Yeung <yeungjasonj@berkeley.edu>
2018-01-04 02:08:35 -0800

    swapped over to Indeed.com and implemented deletion buttons and starting on local storage

--- manager.js
+++ manager.js
@@ -1,19 +1,52 @@
-//var searchResults = document.getElementsByClassName("results-list ember-view");
-//console.log(searchResults);
-//var resultChildren = searchResults.childElementCount;
-//var peopleElements = document.getElementsByClassName("search-result search-result__occluded-item ember-view");
-var peopleElements = document.getElementsByClassName("search-result__info pt3 pb4 ph0");
-console.log(peopleElements);
+var searchResults = document.getElementsByClassName("sre");
 // For each Person in the search results
-console.log(peopleElements.length);
-for (var i = 0; i < peopleElements.length; i++) {
-    // The linkedIn URL of the person we are currently examining.
-    var innerHTML = peopleElements[i].innerHTML;
-    if (innerHTML.includes("/in/ACoAAAawfEQBEVKI-z6lOabBKcACH7iMWAgq6MU/")) {
-        console.log("This should be Robert C Chang");
-        console.log(innerHTML.includes("Robert"));
+console.log("Number of candidates: " + searchResults.length);
+var resultsList = document.getElementById("results");
+for (var i = 0; i < searchResults.length;) {
+    // Check if the current person is blacklisted. If so, delete entry
+    i = matchAndDelete(searchResults[i], i);
+}
+console.log("New length of Search Results after deleting matches: " + searchResults.length);
+// Attach buttons to all remaining entries
+for (var i = 0; i < searchResults.length; i++) {
+    //The linkedIn URL of the person we are currently examining.
+    var currentPerson = searchResults[i];
+    // Create and attach a button to the current candidate entry
+    attachButton(currentPerson);
+}
+
+// Attach a button to the searchEntry to allow deletion of the entry
+function attachButton(searchEntry) {
+    var button = document.createElement("button");
+    button.innerHTML = "Don't see this entry again";
+    searchEntry.appendChild(button);
+    button.addEventListener ("click", function(event) {
+        saveAndRemove(event);
+    });
+
+}
+
+// Take in a searchEntry of a candidate and check if we have already seen candidate.
+// If so, delete the entry. Returns the index for the next person we should check.
+function matchAndDelete(searchEntry, elemCounter) {
+    var elemInnerHTML = searchEntry.innerHTML;
+    //var elementID = searchEntry.id;
+    var elementID = "gibberish";
+    if (elemInnerHTML.includes(elementID)) {
+        searchEntry.remove();
+        console.log("Deleting Entry");
+        return elemCounter;
     } else {
-        console.log("Not Robert C Chang");
+        console.log("No matches found");
+        return elemCounter + 1;
     }
-    
+}
+
+// Called from a mouse event to delete the clicked button's candidate and add to blacklist
+function saveAndRemove(event) {
+    console.log(event);
+    var currElem = event.path[1];
+    // Save the ID of the element that we want to remove
+    window.localStorage.setItem(currElem.id, 'true');
+    currElem.remove();
 }
